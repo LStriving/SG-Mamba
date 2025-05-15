e257420f89656c19c70fa23f3e4b0353078c354f
LStriving <2625847986@qq.com>
2025-05-15 22:37:23 +0800

    add divider

--- src/components/sections/Markdown.vue
+++ src/components/sections/Markdown.vue
@@ -16,6 +16,8 @@ import page from '../mds/md.mdx';
         />
       </el-col>
     </el-row>
+    <el-divider />
+
     <el-row justify="center">
         <el-col :xs="24" :sm="22" :md="18" :lg="16" :xl="16">
             <page />
